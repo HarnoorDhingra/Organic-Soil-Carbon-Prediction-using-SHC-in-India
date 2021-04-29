dd4ce82f83a1c7f16f171f0a143e33b8f648acf5
Harnoor Dhingra <harnoordhingra@gmail.com>
2021-04-29 23:14:17 +0530

    First Commit

---
+++ shc_crawler.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.support.ui import Select
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
+import os,glob,shutil
+import pickle
+from selenium.webdriver.common.keys import Keys
